51811ff86b9a1b0bc0d5eb3949004cd6b9e8dcce
Jeremy Kale Padot <jeremy@padot.us>
2020-03-06 11:12:50 -0800

    fail test. word and sentence are reconized however, fails if sentence has repeats

--- WordCounter.Tests.cs
+++ WordCounter.Tests.cs
@@ -1,11 +1,26 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using Counter.Models;
 using System;
 
 namespace Counter.Tests
 {
   [TestClass]
+  // change class name?
   public class MakeFilesTests
   {
+    [TestMethod]
+    public void InputConstructor_CreatesInstanceOfUserInput_UserInput()
+    {
+      // arrange
+      string userWord = "cat";
+      string userSentence = "cat cat cat";
+      UserInput newUserInput = new UserInput(userWord, userSentence);
+      // List<string> list1 = new List<string> {"I love my cat"};
+      // UserInput newUserInput = new UserInput(string1, list1);
+      // act
+      // assert
+      Assert.AreEqual(userWord, userSentence);
+    }
   }
 }
