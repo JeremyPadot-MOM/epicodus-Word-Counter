add2480d495d53df42e4897414c91da4ab0a5f43
Jeremy Kale Padot <jeremy@padot.us>
2020-03-06 09:45:04 -0800

    add welcome message and score response

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -1,13 +1,18 @@
 using System;
 using Counter.Models;
 
-namespace Game
+namespace Counter
 {
   public class Program
   {
     public static void Main()
     {
+      Console.WriteLine("Please enter a word, followed by a sentence containing that word and I'll tell you how many times it appears.");
+      string response = Console.ReadLine();
+      Word newResponse = new Word(response);
 
+      in score = newResponse.GetScore();
+      Console.WriteLine("your word appeared" + score + "times.");
     }
   }
 }
